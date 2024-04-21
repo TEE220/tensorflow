e738a41d7519d5dbcec954ac067d7ac799657596
Eugene Zhulenev <ezhulenev@google.com>
2024-04-21 16:49:27 -0700

    [pjrt] Switch TfrtTpuBuffer to PjRtFuture<>::Promise to track definition event
    In preparation for disabling absl::Status payloads for AsyncValue convert PjRtClient to PjRtFuture
    
    PiperOrigin-RevId: 626866206

--- third_party/xla/xla/pjrt/pjrt_future.h
+++ third_party/xla/xla/pjrt/pjrt_future.h
@@ -184,6 +184,8 @@ class PjRtFutureBase {
       return ref_.CopyRCRef();
     }
 
+    tsl::AsyncValue* GetAsyncValue() const { return ref_.GetAsyncValue(); }
+
    private:
     tsl::AsyncValueRef<T> ref_;
   };
@@ -243,6 +245,9 @@ class PjRtFutureBase {
 template <class T>
 class PjRtFuture : public internal::PjRtFutureBase<T> {
   using Base = internal::PjRtFutureBase<T>;
+
+  static_assert(!std::is_same_v<T, absl::Status>,
+                "Use PjRtFuture<> specialization for stateless futures");
 
  public:
   // Wrapper for AsyncValueRef<T> that can be used by clients that don't
@@ -359,6 +364,9 @@ class PjRtFuture<void> : public internal::PjRtFutureBase<std::nullopt_t> {
     // value stored in the AsyncValue.
     using Base::Promise::CopyRCRef;
 
+    // Same as above but returns non-owned pointer to underlying AsyncValue.
+    using Base::Promise::GetAsyncValue;
+
     // Sets the promise completed with a given status. Must be called at most
     // once.
     //
@@ -418,32 +426,6 @@ class PjRtFuture<void> : public internal::PjRtFutureBase<std::nullopt_t> {
     }
     return Base::promise().IsError() ? Base::promise().GetError()
                                      : absl::OkStatus();
-  }
-
-  // TODO(b/333538339): Remove when all users of PjRtFuture<Status> will be
-  // converted to PjRtFuture<>. Currently this is an escape hatch to convert
-  // implicit error of a stateless event to a stateful future.
-  PjRtFuture<absl::Status> ToStatusFuture() {
-    auto promise = PjRtFuture<absl::Status>::CreatePromise();
-    OnReady([promise](absl::Status status) mutable {
-      promise.Set(std::move(status));
-    });
-    return PjRtFuture<absl::Status>(std::move(promise));
-  }
-
-  // TODO(b/333538339): Remove when all users of PjRtFuture<Status> will be
-  // converted to PjRtFuture<>. Currently this is an escape hatch to convert
-  // explicit error carried in a stateful future to a stateless future.
-  static PjRtFuture<> FromStatusFuture(PjRtFuture<absl::Status> future) {
-    PjRtFuture<>::Promise promise = PjRtFuture<>::CreatePromise();
-    future.OnReady([promise](absl::Status status) mutable {
-      if (status.ok()) {
-        promise.Set();
-      } else {
-        promise.SetError(std::move(status));
-      }
-    });
-    return PjRtFuture<>(std::move(promise));
   }
 
   // Registers callback to be called once the future is ready.
